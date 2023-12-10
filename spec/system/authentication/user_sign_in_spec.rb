ce9fe4f097368e047ed6fb7cc73e6c211f458393
Valéria Carneiro <valerialribeiroc@gmail.com>
2023-12-10 20:44:36 -0300

    Cadastro de usuário

--- spec/system/authentication/user_sign_in_spec.rb
+++ spec/system/authentication/user_sign_in_spec.rb
@@ -22,4 +22,27 @@ describe 'Usuário se autentica' do
       expect(page).to have_content 'João - joao@email.com'
     end
   end
+
+  it 'e faz logout' do
+    # Arrange
+    User.create!(name: 'João', email: 'joao@email.com', password: 'password')
+
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    within('main form') do
+      fill_in 'E-mail', with: 'joao@email.com'
+      fill_in 'Senha', with: 'password'
+      click_on 'Entrar'
+    end
+    click_on 'Sair'
+
+    # Assert
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    within('nav') do
+      expect(page).to have_link 'Entrar'
+      expect(page).not_to have_button 'Sair'
+      expect(page).not_to have_content 'joao@email.com'
+    end
+  end
 end
